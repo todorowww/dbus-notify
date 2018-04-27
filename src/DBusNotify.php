7a965921d13edb313b422e5c51e77acd6b1694f7
Slobodan Todorov <slobodan@todorowww.net>
2018-04-27 17:21:44 +0200

    Added comments, removed website reference from composer.json

--- src/DBusNotify.php
+++ src/DBusNotify.php
@@ -7,9 +7,24 @@ use Exception;
 class DBusNotify
 {
 
+	/**
+	 * Contains Dbus instance
+	 * 
+	 * @var object 
+	 */
     private $dbus;
+	/**
+	 * Contains Dbus proxy instance
+	 * 
+	 * @var object
+	 */
     private $proxy;
 
+	/**
+	 * Initializes Dbus class, and sets up proxy for notifications
+	 * 
+	 * @throws Exception
+	 */
     public function __construct()
     {
         if (!class_exists("Dbus", false)) {
@@ -25,6 +40,16 @@ class DBusNotify
         $this->dbus->addWatch("org.freedesktop.Notifications", "NotificationClosed");
     }
 
+	/**
+	 * Sends D-Bus notification
+	 * 
+	 * @param string $appName Your application name
+	 * @param string $appIcon Location to your icon, or system icon name
+	 * @param string $notificationSummary Notification title
+	 * @param string $notificationBody Notification message
+	 * @param array $notificationActions Array of actions (buttons) to add to notification
+	 * @param int $timeout Timeout until notification expires (in ms).
+	 */
     public function notify($appName, $appIcon, $notificationSummary, $notificationBody, $notificationActions = [], $timeout = 5000)
     {
         $actions = [];
@@ -44,6 +69,12 @@ class DBusNotify
         );
     }
 
+	/**
+	 * Waits for notification feedback, if actions were supplied
+	 * 
+	 * @param callback $callback Callback to trigger when action happens
+	 * @param int $timeout Number of seconds to wait for signal, before exiting
+	 */
     public function waitForAction($callback = null, $timeout = 5)
     {
         $timedout = false;
